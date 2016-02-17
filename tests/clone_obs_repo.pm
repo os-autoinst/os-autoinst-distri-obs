af6f435f9e0d8ccb44c73bcf744ba9ddd50b29fe
Shyukri Shyukriev <shyukri@e540.ixen-bg>
2016-02-17 17:09:05 +0200

    Use correct repo and remove some debugging.

--- tests/clone_obs_repo.pm
+++ tests/clone_obs_repo.pm
@@ -4,10 +4,7 @@ use testapi;
 
 sub run() {
 	script_run("echo -en \"[client]\nuser = root\npassword = opensuse\n\" > /root/.my.cnf");
-	assert_script_run 'cat /root/.my.cnf';
-	script_run("ip a");
-	script_run("cat /etc/resolv.conf");
-	assert_script_run "git clone https://github.com/M0ses/open-build-service.git /tmp/open-build-service";
+	assert_script_run "git clone https://github.com/openSUSE/open-build-service.git /tmp/open-build-service";
 	assert_script_run "make -C  /tmp/open-build-service/dist test_system"
 	}
 

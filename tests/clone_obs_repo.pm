c865baa206f623dd42e4562472115f251c482813
Shyukri Shyukriev <shshyukriev@suse.com>
2016-03-03 14:50:13 +0200

    Speed up cloning the repo and increase timeout.

--- tests/clone_obs_repo.pm
+++ tests/clone_obs_repo.pm
@@ -4,7 +4,7 @@ use testapi;
 
 sub run() {
 	script_run("echo -en \"[client]\nuser = root\npassword = opensuse\n\" > /root/.my.cnf");
-	assert_script_run "git clone https://github.com/openSUSE/open-build-service.git /tmp/open-build-service";
+	assert_script_run "git clone --single-branch --depth 1 https://github.com/openSUSE/open-build-service.git /tmp/open-build-service", 200;
 	assert_script_run "make -C  /tmp/open-build-service/dist test_system"
 	}
 

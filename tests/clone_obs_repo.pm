c1f7339b5f19b4c9728da6670d8c85409b613b91
Shyukri Shyukriev <shshyukriev@suse.com>
2016-03-08 16:50:20 +0100

    Increase timeout for running the services checks. Weneed this since systemd startup is async.

--- tests/clone_obs_repo.pm
+++ tests/clone_obs_repo.pm
@@ -5,7 +5,7 @@ use testapi;
 sub run() {
 	script_run("echo -en \"[client]\nuser = root\npassword = opensuse\n\" > /root/.my.cnf");
 	assert_script_run "git clone --single-branch --depth 1 https://github.com/openSUSE/open-build-service.git /tmp/open-build-service", 200;
-	assert_script_run "make -C /tmp/open-build-service/dist test_system | tee /tmp/test_system.txt";
+	assert_script_run "make -C /tmp/open-build-service/dist test_system | tee /tmp/test_system.txt", 600;
     upload_logs "/tmp/test_system.txt";
 	}
 

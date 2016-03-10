b6ad26704c4cacc64d8d12c2e8d75021a3607b50
Shyukri Shyukriev <shshyukriev@suse.com>
2016-03-10 21:25:55 +0100

    Move all installations before executing appliance tests via prove which is available by obs-tests-appplaince pkg, so systemd will have time to start the services (hopefully).

--- tests/rspec_webui_tests.pm
+++ tests/rspec_webui_tests.pm
@@ -3,12 +3,9 @@ use strict;
 use testapi;
 
 sub run() {
-	assert_script_run("cd ~ ; rm -rf /tmp/open-build-service");
-	assert_script_run("zypper -n --no-gpg-checks ref -s", 300);
-	assert_script_run("zypper -n in obs-api-testsuite-deps ruby2.3-devel libxml2-devel libxslt-devel", 200);
-	assert_script_run("git clone --single-branch --branch master --depth 1 https://github.com/shyukri/open-build-service.git  /tmp/open-build-service", 200);
+	assert_script_run("git clone --single-branch --branch signup_signin_tests --depth 1 https://github.com/shyukri/open-build-service.git  /tmp/open-build-service", 200);
 	assert_script_run("cd /tmp/open-build-service/dist/t"); # we don't need bundle install in the appliance!
-	assert_script_run("rspec");
+	assert_script_run("rspec --format documentation");
 	}
 
 sub test_flags() {

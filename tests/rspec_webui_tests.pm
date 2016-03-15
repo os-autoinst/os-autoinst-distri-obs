d25e3036c3c8aa657eca2a7f47edb2132680cf45
Shyukri Shyukriev <shshyukriev@suse.com>
2016-03-15 22:06:18 +0200

    Log rspec output.

--- tests/rspec_webui_tests.pm
+++ tests/rspec_webui_tests.pm
@@ -5,11 +5,12 @@ use testapi;
 sub run() {
 	assert_script_run("git clone --single-branch --branch signup_signin_tests --depth 1 https://github.com/shyukri/open-build-service.git  /tmp/open-build-service", 200);
 	assert_script_run("cd /tmp/open-build-service/dist/t"); # we don't need bundle install in the appliance!
-	assert_script_run("rspec --format documentation");
+	script_run("rspec --format documentation && touch /tmp/SUCC | tee /tmp/rspec_tests.txt", 360);
+	upload_logs("/tmp/rspec_tests.txt");
 	}
 
 sub test_flags() {
-    return {important => 1};
+	return {important => 1};
 	}
 
 1;
--- tests/trigger_obs_appliance_tests.pm
+++ tests/trigger_obs_appliance_tests.pm
@@ -3,9 +3,9 @@ use strict;
 use testapi;
 
 sub run() {
-	assert_script_run("zypper -n --no-gpg-checks ref -s", 300);
+	assert_script_run("zypper -n --no-gpg-checks ref -s", 600);
 	#assert_script_run("zypper -n dup", 200);
-	assert_script_run("zypper -n in obs-tests-appliance obs-api-testsuite-deps ruby2.3-devel libxml2-devel libxslt-devel", 200);
+	assert_script_run("zypper -n in obs-tests-appliance obs-api-testsuite-deps ruby2.3-devel libxml2-devel libxslt-devel", 300);
 	script_run("echo -en \"[client]\nuser = root\npassword = opensuse\n\" > /root/.my.cnf");
 	script_run("prove -v /usr/lib/obs/tests/appliance/*.{t,ts} && touch /tmp/SUCC | tee /tmp/appliance_tests.txt", 360);
 	upload_logs("/tmp/appliance_tests.txt")

70663ca33f25bdfbf4a44c2aacbb8207038f3a10
Shyukri Shyukriev <shshyukriev@suse.com>
2016-03-21 08:19:34 +0200

    User bash pipefail trying to overcome log file creation if tests fails.

--- tests/rspec_webui_tests.pm
+++ tests/rspec_webui_tests.pm
@@ -5,7 +5,7 @@ use testapi;
 sub run() {
 	assert_script_run("git clone --single-branch --branch signup_signin_tests --depth 1 https://github.com/shyukri/open-build-service.git  /tmp/open-build-service", 200);
 	assert_script_run("cd /tmp/open-build-service/dist/t"); # we don't need bundle install in the appliance!
-	script_run("rspec --format documentation && touch /tmp/SUCC | tee /tmp/rspec_tests.txt", 360);
+	script_run("set -o pipefail; rspec --format documentation | tee /tmp/rspec_tests.txt", 360);
 	upload_logs("/tmp/rspec_tests.txt");
 	}
 

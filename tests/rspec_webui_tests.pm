1a74df02ed0c19ebb09d6a8bc70aa4f06ee5c171
Shyukri Shyukriev <shshyukriev@suse.com>
2016-03-22 09:58:18 +0200

    Use assert_script_run to get the exit status of failing rspec test

--- tests/rspec_webui_tests.pm
+++ tests/rspec_webui_tests.pm
@@ -5,7 +5,7 @@ use testapi;
 sub run() {
 	assert_script_run("git clone --single-branch --branch signup_signin_tests --depth 1 https://github.com/shyukri/open-build-service.git  /tmp/open-build-service", 200);
 	assert_script_run("cd /tmp/open-build-service/dist/t"); # we don't need bundle install in the appliance!
-	script_run("set -o pipefail; rspec --format documentation | tee /tmp/rspec_tests.txt", 360);
+	assert_script_run("set -o pipefail; rspec --format documentation | tee /tmp/rspec_tests.txt", 360);
 	save_screenshot;
 	upload_logs("/tmp/rspec_tests.txt");
 	}

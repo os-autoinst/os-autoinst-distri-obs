5daf104ca5515d45a93e15c7c0f0a1f854fc862a
Shyukri Shyukriev <shshyukriev@suse.com>
2016-03-31 13:22:04 +0300

    Do not install obs-api-testsuite-deps package. Looks like it's not required for tests.

--- tests/trigger_obs_appliance_tests.pm
+++ tests/trigger_obs_appliance_tests.pm
@@ -4,7 +4,7 @@ use testapi;
 
 sub run() {
 	assert_script_run("zypper -n --no-gpg-checks ref -s", 400);
-	assert_script_run("zypper -n in --force-resolution --no-recommends obs-tests-appliance obs-api-testsuite-deps ruby2.3-devel libxml2-devel libxslt-devel", 300);
+	assert_script_run("zypper -n in --force-resolution --no-recommends obs-tests-appliance ruby2.3-devel libxml2-devel libxslt-devel", 300);
 	script_run("echo -en \"[client]\nuser = root\npassword = opensuse\n\" > /root/.my.cnf");
 	my $script="prove -v /usr/lib/obs/tests/appliance/*.{t,ts} 2>&1 | tee /tmp/appliance_tests.txt", 360;
 	validate_script_output $script, sub { m/All tests successful./ };

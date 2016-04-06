ab8031baa247646b557b4dc1d8661de0212ab831
Shyukri Shyukriev <shshyukriev@suse.com>
2016-04-06 20:31:52 +0300

    Use tar.bz2 when archiving logs.

--- tests/rspec_webui_tests.pm
+++ tests/rspec_webui_tests.pm
@@ -15,11 +15,11 @@ sub test_flags() {
 
 sub post_fail_hook {
 	upload_logs("/tmp/rspec_tests.txt");
-	assert_script_run("tar -cvf /tmp/capybara_screens.tar.gz /tmp/rspec_screens/*");
-	upload_logs("/tmp/capybara_screens.tar.gz");
-	assert_script_run("tar -cvf /tmp/srv_www_obs_api_logs.tar.gz /srv/www/obs/api/log/*");
-	upload_logs("/tmp/srv_www_obs_api_logs.tar.gz");
-	assert_script_run("tar -cvf /tmp/build_home_Admin.tar.gz /srv/obs/build/*");
+	assert_script_run("tar cjf /tmp/capybara_screens.tar.bz2 /tmp/rspec_screens/*");
+	upload_logs("/tmp/capybara_screens.tar.bz2");
+	assert_script_run("tar cjf /tmp/srv_www_obs_api_logs.tar.bz2 /srv/www/obs/api/log/*");
+	upload_logs("/tmp/srv_www_obs_api_logs.tar.bz2");
+	assert_script_run("tar cjf /tmp/build_home_Admin.tar.bz2 /srv/obs/build/*");
 	upload_logs("/tmp/build_home_Admin.tar.gz");
 	}
 1;

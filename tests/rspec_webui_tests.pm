095df73cd8c5b60d5d5591afd4efac8eb854cea5
Shyukri Shyukriev <shshyukriev@suse.com>
2016-04-07 23:09:28 +0300

    Fix uploading logs for /srv/obs/build/*

--- tests/rspec_webui_tests.pm
+++ tests/rspec_webui_tests.pm
@@ -5,7 +5,7 @@ use testapi;
 sub run() {
 	assert_script_run("git clone --single-branch --branch rspec_appliance_tests_fix --depth 1 https://github.com/shyukri/open-build-service.git  /tmp/open-build-service", 200);
 	assert_script_run("cd /tmp/open-build-service/dist/t"); # we don't need bundle install in the appliance!
-	assert_script_run("set -o pipefail; rspec --format documentation | tee /tmp/rspec_tests.txt", 360);
+	assert_script_run("set -o pipefail; rspec --format documentation | tee /tmp/rspec_tests.txt", 480);
 	save_screenshot;
 	}
 
@@ -19,7 +19,7 @@ sub post_fail_hook {
 	upload_logs("/tmp/capybara_screens.tar.bz2");
 	assert_script_run("tar cjf /tmp/srv_www_obs_api_logs.tar.bz2 /srv/www/obs/api/log/*");
 	upload_logs("/tmp/srv_www_obs_api_logs.tar.bz2");
-	assert_script_run("tar cjf /tmp/build_home_Admin.tar.bz2 /srv/obs/build/*");
-	upload_logs("/tmp/build_home_Admin.tar.gz");
+	assert_script_run("tar cjf /tmp/srv_obs_build.tar.bz2 /srv/obs/build/*");
+	upload_logs("/tmp/srv_obs_build.tar.bz2");
 	}
 1;

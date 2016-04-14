6e68fbd06a60cd4d179aca7fc123588ffcd94e66
Shyukri Shyukriev <shshyukriev@suse.com>
2016-04-14 11:43:12 +0300

    Add .ta tests (test appliance) after they're splitted.

--- tests/trigger_obs_appliance_tests.pm
+++ tests/trigger_obs_appliance_tests.pm
@@ -6,7 +6,7 @@ sub run() {
 	assert_script_run("zypper -n --no-gpg-checks ref -s", 400);
 	assert_script_run("zypper -n in --force-resolution --no-recommends obs-tests-appliance ruby2.3-devel libxml2-devel libxslt-devel", 300);
 	script_run("echo -en \"[client]\nuser = root\npassword = opensuse\n\" > /root/.my.cnf");
-	my $script="prove -v /usr/lib/obs/tests/appliance/*.{t,ts} 2>&1 | tee /tmp/appliance_tests.txt", 360;
+	my $script="prove -v /usr/lib/obs/tests/appliance/*.{t,ts,ta} 2>&1 | tee /tmp/appliance_tests.txt", 360;
 	validate_script_output $script, sub { m/All tests successful./ };
 	save_screenshot;
 	upload_logs("/tmp/appliance_tests.txt");

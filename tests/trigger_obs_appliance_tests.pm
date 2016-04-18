e6c76dbbceb38b4886f33391033bd463acf5ca63
Shyukri Shyukriev <shshyukriev@suse.com>
2016-04-18 23:54:54 +0300

    Add verbosity to zypper. We've trouble refreshing the repos.

--- tests/trigger_obs_appliance_tests.pm
+++ tests/trigger_obs_appliance_tests.pm
@@ -3,8 +3,8 @@ use strict;
 use testapi;
 
 sub run() {
-	assert_script_run("zypper -n --no-gpg-checks ref -s", 400);
-	assert_script_run("zypper -n in --force-resolution --no-recommends obs-tests-appliance ruby2.3-devel libxml2-devel libxslt-devel", 300);
+	assert_script_run("zypper -vv -n --no-gpg-checks ref -s", 500);
+	assert_script_run("zypper -vv -n in --force-resolution --no-recommends obs-tests-appliance ruby2.3-devel libxml2-devel libxslt-devel", 300);
 	script_run("echo -en \"[client]\nuser = root\npassword = opensuse\n\" > /root/.my.cnf");
 	my $script="prove -v /usr/lib/obs/tests/appliance/*.{t,ts,ta} 2>&1 | tee /tmp/appliance_tests.txt";
 	validate_script_output $script, sub { m/All tests successful./ }, 360;

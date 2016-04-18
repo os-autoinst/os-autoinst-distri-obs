3de336fd328938ae25467c258008250a26f8fec8
Shyukri Shyukriev <shshyukriev@suse.com>
2016-04-18 17:40:11 +0300

    Move prove timeout to validate_script_output method.

--- tests/trigger_obs_appliance_tests.pm
+++ tests/trigger_obs_appliance_tests.pm
@@ -6,8 +6,8 @@ sub run() {
 	assert_script_run("zypper -n --no-gpg-checks ref -s", 400);
 	assert_script_run("zypper -n in --force-resolution --no-recommends obs-tests-appliance ruby2.3-devel libxml2-devel libxslt-devel", 300);
 	script_run("echo -en \"[client]\nuser = root\npassword = opensuse\n\" > /root/.my.cnf");
-	my $script="prove -v /usr/lib/obs/tests/appliance/*.{t,ts,ta} 2>&1 | tee /tmp/appliance_tests.txt", 360;
-	validate_script_output $script, sub { m/All tests successful./ };
+	my $script="prove -v /usr/lib/obs/tests/appliance/*.{t,ts,ta} 2>&1 | tee /tmp/appliance_tests.txt";
+	validate_script_output $script, sub { m/All tests successful./ }, 360;
 	save_screenshot;
 	upload_logs("/tmp/appliance_tests.txt");
 	}

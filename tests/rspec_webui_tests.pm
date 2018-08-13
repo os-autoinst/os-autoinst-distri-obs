df859c963b99674c1dab053a5c511f63c55daaf9
Saray Cabrera Padrón <scabrerapadron@suse.de>
2018-08-13 13:05:13 +0200

    Replace phantomjs by chromedriver only in Unstable versions
    Co-authored-by: Eduardo Navarro <enavarro@suse.com>

--- tests/rspec_webui_tests.pm
+++ tests/rspec_webui_tests.pm
@@ -4,8 +4,10 @@ use testapi;
 
 sub run() {
     my $branch = "master";
+    my $tests_packages = "chromedriver xorg-x11-fonts";
     if (get_var('VERSION') != "Unstable") {
         $branch = get_var('VERSION');
+        $tests_packages = "phantomjs";
     }
 
     my $ruby_version = "2.5";
@@ -15,7 +17,7 @@ sub run() {
 
     assert_script_run("git clone --single-branch --branch $branch --depth 1 https://github.com/openSUSE/open-build-service.git  /tmp/open-build-service", 240);
     assert_script_run("cd /tmp/open-build-service/dist/t");
-    assert_script_run("zypper -vv -n --gpg-auto-import-keys in --force-resolution --no-recommends chromedriver libxml2-devel libxslt-devel ruby$ruby_version-devel", 600);
+    assert_script_run("zypper -vv -n --gpg-auto-import-keys in --force-resolution --no-recommends $tests_packages libxml2-devel libxslt-devel ruby$ruby_version-devel", 600);
     assert_script_run("bundle.ruby$ruby_version install", 600);
     assert_script_run("set -o pipefail; bundle.ruby$ruby_version exec rspec --format documentation | tee /tmp/rspec_tests.txt", 600);
     save_screenshot;

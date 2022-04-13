0f78bedd07b1bf56cae8f5268ec51269babcdb5e
Henne Vogelsang <hvogel@opensuse.org>
2022-04-13 19:55:56 +0200

    Move installation etc. to a Makefile in the branch
    Make it easier to track changes in one place.
    We switched to Ruby 3.1 in Unstable and Ruby 2.7 in 2.10...

--- tests/rspec_webui_tests.pm
+++ tests/rspec_webui_tests.pm
@@ -4,20 +4,15 @@ use testapi;
 
 sub run() {
     my $branch = "master";
-    my $tests_packages = "chromedriver xorg-x11-fonts";
     if (get_var('VERSION') != "Unstable") {
         $branch = get_var('VERSION');
-        $tests_packages = "phantomjs" unless get_var('VERSION') == "2.10";
     }
 
-    my $ruby_version = "2.5";
-
-    assert_script_run("zypper -vv -n --gpg-auto-import-keys in --force-resolution --no-recommends $tests_packages libxml2-devel libxslt-devel ruby$ruby_version-devel", 600);
-    assert_script_run("git clone --single-branch --branch $branch --depth 1 https://github.com/openSUSE/open-build-service.git /tmp/open-build-service", 240);
+    assert_script_run("curl -O https://raw.githubusercontent.com/openSUSE/open-build-service/$branch/dist/t/Makefile", 240);
+    assert_script_run("make install", 840);
     assert_script_run("cd /tmp/open-build-service/dist/t");
     assert_script_run("set -o pipefail; prove -v *-check_required_services.ts | tee /tmp/check_required_services_tests.txt", 300);
-    assert_script_run("bundle.ruby$ruby_version install", 600);
-    assert_script_run("set -o pipefail; bundle.ruby$ruby_version exec rspec --format documentation | tee /tmp/rspec_tests.txt", 600);
+    assert_script_run("set -o pipefail; bundle exec rspec --format documentation | tee /tmp/rspec_tests.txt", 600);
     save_screenshot;
     upload_logs("/tmp/check_required_services_tests.txt");
     upload_logs("/tmp/rspec_tests.txt");
